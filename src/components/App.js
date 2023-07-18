669316ea1412161d29c23a52b91a88ebf14afbdb
sajid <mohdsajid8010@gmail.com>
2023-07-18 13:49:15 +0530

    comiit1

--- src/components/App.js
+++ src/components/App.js
@@ -2,10 +2,18 @@
 import React from "react";
 import './../styles/App.css';
 
+let weather_inp = { temperature: 25, conditions: "Sunny" };
+
 const App = () => {
   return (
     <div>
-        {/* Do not remove the main div */}
+      {/* Do not remove the main div */}
+      {
+        weather_inp.temperature > 20 ?
+          (<div style={{ color: "red" }}>{weather_inp.temperature}</div>)
+          : (<div style={{ color: "blue" }}>{weather_inp.temperature}</div>)
+      }
+      <div>{weather_inp.conditions}</div>
     </div>
   )
 }
